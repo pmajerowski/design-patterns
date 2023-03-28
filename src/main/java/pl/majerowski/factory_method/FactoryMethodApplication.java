62f8d997a30c75704258c9266a22fd1f3585a084
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-28 11:59:13 +0200

    Factory Method basic implementation

--- src/main/java/pl/majerowski/factory_method/FactoryMethodApplication.java
+++ src/main/java/pl/majerowski/factory_method/FactoryMethodApplication.java
@@ -2,12 +2,20 @@ package pl.majerowski.factory_method;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.majerowski.factory_method.logistics.Logistics;
+import pl.majerowski.factory_method.transport.Ship;
+import pl.majerowski.factory_method.transport.Transport;
+import pl.majerowski.factory_method.transport.Truck;
 
 @SpringBootApplication
 public class FactoryMethodApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FactoryMethodApplication.class, args);
+        Transport truck = new Truck();
+        truck.deliver();
+
+        Transport ship = new Ship();
+        ship.deliver();
     }
 
 }
